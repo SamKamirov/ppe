efd954f73ce36766074565fa66fe02df498c73f9
Sam <semkamirov@gmail.com>
2024-06-01 16:27:40 +0500

    Изменяет размер бокового меню

--- src/components/table-row/employee-table-row.tsx
+++ src/components/table-row/employee-table-row.tsx
@@ -9,6 +9,20 @@ export const EmployeeTableRow = ({ employee }: Props) => {
     return (
         <tr className='text-center table-row'>
             <td className='text-center align-middle'>{employee.id}</td>
+            <td className='text-center align-middle'>{employee.personellNumber}</td>
+            <td className='text-center align-middle'>{employee.firstName}</td>
+            <td className='text-center align-middle'>{employee.middleName}</td>
+            <td className='text-center align-middle'>{employee.lastName}</td>
+            <td className='text-center align-middle'>{employee.structuralUnit}</td>
+            <td className='text-center align-middle'>{employee.position}</td>
+            <td className='text-center align-middle'>{employee.enterDate}</td>
+            <td className='text-center align-middle'>{employee.changePositionDate}</td>
+            <td className='text-center align-middle'>{employee.sex}</td>
+            <td className='text-center align-middle'>{employee.height}</td>
+            <td className='text-center align-middle'>{employee.clothesSize}</td>
+            <td className='text-center align-middle'>{employee.shoeSize}</td>
+            <td className='text-center align-middle'>{employee.headwearSize}</td>
+            <td className='text-center align-middle'>{employee.gasMaskSize}</td>
         </tr>
     )
 }
--- src/pages/profile/source/profile-sidebar.tsx
+++ src/pages/profile/source/profile-sidebar.tsx
@@ -4,7 +4,7 @@ import { Section } from "../../../components/sidebar/section";
 
 export const ProfileSideBar = () => {
     return (
-        <nav className='sidebar my-0 px-0'>
+        <nav className='sidebar profile-sidebar my-0 px-0'>
             <ul className='list-group'>
                 {profileSideBarElements.map((item) => <Section item={item} key={item.section} />)}
             </ul>
