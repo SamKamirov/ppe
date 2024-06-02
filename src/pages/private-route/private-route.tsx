5a7b3354b5987d860f4987d8f8dd379b534de029
Sam <semkamirov@gmail.com>
2024-06-02 15:52:15 +0500

    Исправляет баги

--- src/pages/private-route/private-route.tsx
+++ src/pages/private-route/private-route.tsx
@@ -1,31 +1,15 @@
-import React, { FC, ReactNode, useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React, { FC, ReactNode } from "react";
+import { Navigate } from "react-router-dom";
 import { AppRoutes } from "../../../const";
-import { User } from "../../types/state";
 import { useAppSelector } from "../../app/hooks";
 import { getUser } from "../../store/user-process/user-process-selectors";
-import { Loading } from "../../components/loading/loading";
-import { getLoadingState } from "../../store/app-data/app-data-selectors";
 
 type Props = {
     children: ReactNode;
 };
 
 export const PrivateRoute: FC<Props> = ({ children }) => {
-    const navigate = useNavigate();
     const user = useAppSelector(getUser);
-    const isLoading = useAppSelector(getLoadingState);
-
-    useEffect(() => {
-        if (!user) {
-            navigate(AppRoutes.Login);
-        };
-    }, [user, navigate]);
-
-
-    if (isLoading) {
-        return <Loading />;
-    };
 
     return (
         user ? children : <Navigate to={AppRoutes.Login} />
